d72e349f6591c7a8d6e710a8ca8d87d166e2135f
luoriwang <574224320@qq.com>
2021-10-01 20:21:00 +0800

    second commit

---
+++ src/main/java/com/lrw/TestGit.java
@@ -0,0 +1,14 @@
+package com.lrw;
+
+/**
+ * @Description: TODO
+ * @author: LuoRiWang@geostar.com.cn
+ * @date: 2021-10-1 20:19
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("test git");
+    }
+    
+}
